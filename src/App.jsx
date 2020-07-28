35842914d11f1dff4536df66bc7b212dd1401de3
gorushkin <gorushkin@gmail.com>
2020-07-28 18:25:01 +0300

    timeout

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useImmer } from 'use-immer';
+import getModal from './modals/index.js';
+
+const App = () => {
+  return <h1>Title</h1>
+}
+
+export default App;
